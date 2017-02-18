0e990215191f10afecb4f07b00fb48bf7fa87cc6
Anton Shurashov <sinkler@sinkler.ru>
2017-02-18 23:02:19 +0300

    removed server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var app = express();
-
-app.set('port', (process.env.PORT || 3000));
-app.use('/', express.static(__dirname));
-app.listen(app.get('port'), function() {
-  console.log('Server started: http://localhost:' + app.get('port') + '/');
-});
